e87d6f2ca0c9bb9d74f5bb44c07dd4d7acf20320
uncle-Stas <stanislav.zavadskyi11@gmail.com>
2023-01-23 01:43:46 +0200

    added email verification

--- controllers/auth/signup.js
+++ controllers/auth/signup.js
@@ -1,7 +1,12 @@
-const { User } = require('../../models/user');
-const { HttpError } = require('../../helpers');
 const bcrypt = require('bcrypt');
 const gravatar = require('gravatar');
+const sendEmail = require('../../helpers/sendMail');
+
+const { nanoid } = require('nanoid');
+const { User } = require('../../models/user');
+const { HttpError } = require('../../helpers');
+
+const { BASE_URL } = process.env;
 
 const signup = async (req, res, next) => {
   try {
@@ -14,8 +19,22 @@ const signup = async (req, res, next) => {
 
     const hashPassword = await bcrypt.hash(password, 10);
     const avatarURL = gravatar.url(email);
+    const verificationToken = nanoid();
 
-    const newUser = await User.create({ ...req.body, avatarURL, password: hashPassword });
+    const newUser = await User.create({
+      ...req.body,
+      avatarURL,
+      verificationToken,
+      password: hashPassword,
+    });
+
+    const verifyEmail = {
+      to: email,
+      subject: 'verify you email',
+      html: `<a target="_blank" href="${BASE_URL}/api/auth/users/verify/${verificationToken}">Click for virifycation youre email</a>`,
+    };
+
+    await sendEmail(verifyEmail);
 
     res.status(201).json({
       name: newUser.name,
